0a8e9a899a69e44dcd3e11d1eff7543d9012c438
malaikakhalid <70518776+malaikakhalid@users.noreply.github.com>
2020-09-25 23:13:02 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1601056648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ice_texture-sheet0.png",
+		"images/logclipart-sheet0.png",
+		"images/hj-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/52a41af6f36fa-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
